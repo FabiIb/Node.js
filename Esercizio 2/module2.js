c31950cb6f23cf34583ff279a212c542f23ce46a
FabiIb <ibrahimifabiola9@gmail.com>
2023-09-08 11:37:24 +0200

    es

---
+++ Esercizio 2/module2.js
@@ -0,0 +1,5 @@
+
+const somma = require('./module1');
+
+const risultato = somma(6,8);
+console.log(risultato);
---
+++ esercizio1.js
@@ -0,0 +1,5 @@
+const crypto = require("crypto");
+const min = 1;
+const max = 100;
+const Id = crypto.randomInt(min, max);
+console.log(Id);
