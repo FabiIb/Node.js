66323b2b4a76da734ab77305fde575fbd172528c
Fabiola <131887709+FabiIb@users.noreply.github.com>
2023-10-08 21:46:07 +0200

    fix

--- Esercizio 11/app.js
+++ Esercizio 11/app.js
@@ -1,24 +1,21 @@
-// Import necessary modules
+
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const planetRouter = require('./planetsRouter'); // Import your planet router
+const planetRouter = require('./planetsRouter'); 
 
-// Create an Express application
+
 const app = express();
 
-// Middleware to parse JSON requests
+
 app.use(express.json());
 
-// Middleware to log client requests using morgan
 app.use(morgan('dev'));
 
-// Use the planet router
+
 app.use(planetRouter);
 
-// ...
 
-// Start the Express server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
