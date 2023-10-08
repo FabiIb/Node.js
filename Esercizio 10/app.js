71f545896d6b22106567b93202d98b2f5657c257
Fabiola <131887709+FabiIb@users.noreply.github.com>
2023-10-08 21:44:50 +0200

    fix

--- Esercizio 10/app.js
+++ Esercizio 10/app.js
@@ -1,18 +1,18 @@
-// Import necessary modules
+
 const express = require('express');
 require('dotenv').config();
 const morgan = require('morgan');
 
-// Create an Express application
+
 const app = express();
 
-// Middleware to parse JSON requests
+
 app.use(express.json());
 
-// Middleware to log client requests using morgan
+
 app.use(morgan('dev'));
 
-// Dummy database with initial data
+
 let planets = [
   {
     id: 1,
@@ -24,12 +24,12 @@ let planets = [
   },
 ];
 
-// Define a route to get all planets
+
 app.get('/planets', (req, res) => {
   res.json(planets);
 });
 
-// Start the Express server
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
