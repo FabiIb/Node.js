53d7aeeb7b1b4be28cb9d1a3f8336c8a7ffe153d
FabiIb <ibrahimifabiola9@gmail.com>
2023-10-13 00:24:29 +0200

    new

--- Esercizio 13/server.mjs
+++ Esercizio 13/server.mjs
@@ -6,20 +6,24 @@ import {
   deleteById,
   getAll,
   getOneById,
-  create,
-  updateById,
+  post,
+  putById,
 } from "./controller.mjs";
 
+const host = process.env.host;
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/api/planets', getAll);
-app.get('/api/planets/:id', getOneById);
-app.post('/api/planets', create);
-app.put('/api/planets/:id', updateById);
-app.delete('/api/planets/:id', deleteById);
+app.get("/planets/:id", getOneById);
+app.get("/planets", getAll);
+
+app.post("/planets", post);
+
+app.put("/planets/:id", putById);
+
+app.delete("/planets/:id", deleteById);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
--- Esercizio 14/server.mjs
+++ Esercizio 14/server.mjs
@@ -1,25 +1,50 @@
 import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
+import multer from "multer";
 
 import {
   deleteById,
   getAll,
   getOneById,
-  create,
-  updateById,
+  post,
+  putById,
 } from "./controller.mjs";
 
 const app = express();
 
-app.use(express.json());
-app.use(morgan('dev'));
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
 
-app.get('/api/planets', getAll);
-app.get('/api/planets/:id', getOneById);
-app.post('/api/planets', create);
-app.put('/api/planets/:id', updateById);
-app.delete('/api/planets/:id', deleteById);
+const upload = multer({ storage: storage });
+
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/planets/:id", getOneById);
+app.get("/planets", getAll);
+
+app.post("/planets", post);
+
+app.put("/planets/:id", putById);
+
+app.delete("/planets/:id", deleteById);
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: 'Nessun file caricato' });
+  }
+
+  res.status(200).json({ message: 'File caricato con successo' });
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
